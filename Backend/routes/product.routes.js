1d5108c33a6c95b63a6744e3f44e4429124429c5
Abhishek Rana <abhishekrana0743@gmail.com>
2026-01-29 14:37:10 +0530

    Add all the Products Rouets

--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -1,7 +1,15 @@
 import express from "express"
+import { getAllProducts,getFeaturedProducts,getRecommendedProducts,createProduct, deleteProduct, getProductByCategory, toggleFeatureProduct } from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router  = express.Router();
 
-router.get("/", getAllProducts)
+router.get("/",protectRoute,adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/recommendations", getRecommendedProducts);
+router.get("/category/:category", getProductByCategory);
+router.post("/", protectRoute,adminRoute,createProduct);
+router.patch("/",protectRoute, adminRoute, toggleFeatureProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router
