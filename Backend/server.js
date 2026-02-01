1a39941d591d14a88951f2f1c1571bb154e78773
Abhishek Rana <abhishekrana0743@gmail.com>
2026-02-01 16:17:09 +0530

    Update the server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,11 @@ import authRoutes from "./routes/auth.routes.js"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import productRoutes from "./routes/product.routes.js"
+import cartRoutes from "./routes/cart.routes.js"
+import coupanRoutes from "./routes/coupan.route.js"
+import paymentRoutes from "./routes/payments.route.js"
+import analyticsRoutes from "./routes/analytic.route.js"
+
 dotenv.config()
 
 const app = express();
@@ -14,6 +19,10 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products",productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupans", coupanRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(5000, () =>{
     console.log("Server is running on http://localhost:" + PORT)
