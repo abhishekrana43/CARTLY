54e1682855d0f59dfa2b684b5ac77f973ade2e3f
Abhishek Rana <abhishekrana0743@gmail.com>
2026-02-01 16:10:20 +0530

    Add the payment model

---
+++ Backend/models/coupan.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const coupanSchema = new mongoose.Schema({
+    code:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    discountPercentage:{
+        type:Number,
+        required:true,
+        min:0,
+        max:100,
+    },
+    expirationDate:{
+        type:Number,
+        required:true,
+
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+
+}, {
+    timestamps: true
+})
+
+const Coupan = mongoose.model("Coupan", coupanSchema);
+
+export default Coupan;
