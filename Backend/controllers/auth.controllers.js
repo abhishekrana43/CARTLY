882b956f8f9f8a4a982f8d03410f6f810639e006
Abhishek Rana <abhishekrana0743@gmail.com>
2026-02-01 16:03:31 +0530

    Update the auth controller

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -148,6 +148,14 @@ export const refreshToken = async (req,res) =>{
         console.log("Error in refreshToken controller", error.message);
         res.status(500).json({message:"Server error", error: error.message});
     }
-}
+};
+
+export const getProfile = async (req, res) => {
+	try {
+		res.json(req.user);
+	} catch (error) {
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
 
 
