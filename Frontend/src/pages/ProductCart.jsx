9a3f22b2054ebf76bfe1e9db230fbbd782e6ce00
Abhishek Rana <abhishekrana0743@gmail.com>
2026-02-10 21:45:24 +0530

    Add the ProductCart Page

---
+++ Frontend/src/pages/ProductCart.jsx
@@ -0,0 +1,43 @@
+import toast from "react-hot-toast";
+import { ShoppingCart } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore.js";
+import { useCartStore } from "../stores/useCartStore.js";
+
+const ProductCart = ({product}) => {
+    const {user} = useUserStore();
+    const {addToCart} = useCartStore();
+    const handleAddToCart = () => {
+        if(user){
+            toast.error("Please login to add products to the cart",{id:"login"});
+            return;
+
+        }
+        else{
+          // add to cart
+           addToCart(product); 
+        }
+    };
+
+    return(
+        <div className="flex w-full relative flex-col overflow-hidden
+        rounded-lg border border-gray-700 shadow-lg">
+            <div className="object-cover w-full" src={product.image} alt='product image'>
+                <div className="absolute inset-0 bg-black bg-opacity-20">
+
+                </div>
+
+                <div className="mt-4 px-5 pb-5">
+                    <h5 className="text-xl font-semibold tracking-tight text-white">
+                        {product.name}
+                    </h5>
+                    <div className="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                        <span className="text-3xl font-bold text-emerald-400">${product.price}</span>
+                    </p>
+                </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
